827155cac947c52d0ac3f1bc94e34dd51fc8d6ea
akbar-cyber <contact.abdullayev@gmail.com>
2020-03-23 14:17:43 +0400

    Updates

---
+++ precache-manifest.6758e6b6269ebdef384b282195e99c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550b2f19c5cd79ae05aa299b0e3fc209",
+    "url": "/Artificial-Intelligence/index.html"
+  },
+  {
+    "revision": "481593d446263fb91a6f",
+    "url": "/Artificial-Intelligence/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6d6d4fc6537ae0fca65b",
+    "url": "/Artificial-Intelligence/static/css/main.f97c2770.chunk.css"
+  },
+  {
+    "revision": "481593d446263fb91a6f",
+    "url": "/Artificial-Intelligence/static/js/2.1eab1359.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Artificial-Intelligence/static/js/2.1eab1359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6d4fc6537ae0fca65b",
+    "url": "/Artificial-Intelligence/static/js/main.f9910edf.chunk.js"
+  },
+  {
+    "revision": "4a4cbc27cbfb0f7bb0a0",
+    "url": "/Artificial-Intelligence/static/js/runtime-main.3ff41f9c.js"
+  }
+]);
